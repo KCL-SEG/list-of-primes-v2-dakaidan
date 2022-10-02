b7f4a1e7075637255b76416cf1f76ae9a57d6561
Aidan Dakhama <ai.dakhama@gmail.com>
2022-10-02 17:49:56 +0100

    add functionality to primes with a seive

--- primes.py
+++ primes.py
@@ -1,6 +1,28 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import math
+
+MAX = 1000000
+
 def primes(number_of_primes):
+    if(number_of_primes <= 0):
+        raise ValueError("number_of_primes must be greater than 0")
+
     list = []
+    prime = [True for i in range(MAX)]
+    prime[1] = False
+
+    for p in range(2, math.ceil(math.sqrt(MAX))):
+        if prime[p]:
+            for i in range(2*p, MAX, p):
+                prime[i] = False
+
+
+    for i in range(2, MAX):
+        if prime[i]:
+            list.append(i)
+            if len(list) == number_of_primes:
+                break
+
     return list
